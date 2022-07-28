8220d0d00ba020a9de8aece6b9f1ef87a761d870
Neo-Machina <andreistatache1997@gmail.com>
2022-07-28 19:38:47 +0200

    exercise completed

---
+++ User.php
@@ -0,0 +1,38 @@
+<?php 
+    class User {
+        public $nome;
+
+        public $email;
+
+        public $sconto = 0;
+
+        protected $prodottiScelti = [];
+
+        public function __construct($_nome, $_email) {
+            $this->nome = $_nome;
+            $this->email = $_email;
+        }
+
+        public function aggiungiProdotti($prodotto) {
+            $this->prodottiScelti[] = $prodotto;
+        }
+
+        public function getProdotti() {
+            return $this->prodottiScelti;
+        }
+
+        public function sommaCostoProdotti() {
+            $somma = 0;
+            foreach($this->prodottiScelti as $prodotto) {
+                $somma += $prodotto->prezzo;
+            }
+
+            // togliamo lo sconto
+            $somma -= ($somma * $this->sconto / 100);
+
+            return $somma;
+        }
+
+        
+    }
+?>
--- index.php
+++ index.php
@@ -9,16 +9,37 @@
     require_once __DIR__ . '/CiboProduct.php';
     require_once __DIR__ . '/ToysProduct.php';
     require_once __DIR__ . '/CucceProduct.php';
+    require_once __DIR__ . '/UserRegistrato.php';
+    require_once __DIR__ . '/UserNonRegistrato.php';
 
     $crocchetteCani = new CiboProduct('Monge', 38.90, 'Natural Superpremium', 12);
     $crocchetteCani->dataScadenza = '12/09/2024';
     $crocchetteCani->tipologiaCibo = 'secco';
 
-    var_dump($crocchetteCani);
+    // var_dump($crocchetteCani);
 
     $ossoGomma = new ToysProduct('Kong', 9.89, 'Extreme Goodie Bone');
-    var_dump($ossoGomma);
+    // var_dump($ossoGomma);
     
     $cucciaOvale = new CucceProduct('Ocean Travel', 36.97, 'Cuccetta Ovale');
-    var_dump( $cucciaOvale);
+    // var_dump( $cucciaOvale);
+
+    // Utente non registarto
+    $micheleGialli = new UserNonRegistrato('Michele Gialli', 'micheleg@email.it');
+    $micheleGialli->aggiungiProdotti($crocchetteCani);
+    $micheleGialli->aggiungiProdotti($ossoGomma);
+    $micheleGialli->aggiungiProdotti($cucciaOvale);
+    var_dump($micheleGialli->sommaCostoProdotti());
+    var_dump($micheleGialli);
+
+    // Utente registrato
+    $seleneRossi = new UserRegistrato('Selene Rossi', 'selenes@email.it');
+    $seleneRossi->aggiungiProdotti($ossoGomma);
+    $seleneRossi->aggiungiProdotti($cucciaOvale);
+    $seleneRossi->aggiungiProdotti($crocchetteCani);
+    var_dump($seleneRossi->sommaCostoProdotti());
+
+    var_dump($seleneRossi);
+
+    
 ?>
