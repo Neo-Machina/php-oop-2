28c9809a61b021f12d93ccbe6f1e68364e612f6a
Neo-Machina <andreistatache1997@gmail.com>
2022-07-28 17:24:54 +0200

    mainanimalproduct class done

---
+++ MainAnimalProduct.php
@@ -0,0 +1,24 @@
+<?php 
+class MainAnimalProduct {
+    public $marca;
+
+    public $prezzo;
+
+    public $quantita;
+
+    public $nome;
+
+    public function __construct($_marca, $_prezzo, $_quantita, $_nome) {
+        $this->marca = $_marca;
+        $this->prezzo = $_prezzo;
+        $this->quantita = $_quantita;
+        $this->nome = $_nome;
+    }
+
+    public function getInfo() {
+        return "$this->marca $this->nome - Quantità: $this->quantita - Prezzo: $this->prezzo";
+    }
+
+    
+}
+?>
